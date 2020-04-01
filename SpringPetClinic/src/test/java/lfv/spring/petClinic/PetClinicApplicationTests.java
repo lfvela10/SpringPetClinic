d59b3cc4d5a5d82ac40fe876ae797f7039998b95
lfvela10 <38299661+lfvela10@users.noreply.github.com>
2020-04-01 14:27:34 -0500

    Add files via upload
    initial folder update

---
+++ SpringPetClinic/src/test/java/lfv/spring/petClinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package lfv.spring.petClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
